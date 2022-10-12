26569831de5cc30d0df9e2ff296ad882b237c715
LeonardoMeireles <leo.meireles01@gmail.com>
2022-10-12 20:36:17 -0300

    fix: test no --strict

--- features/step_definitions/list-streams.rb
+++ features/step_definitions/list-streams.rb
@@ -1,35 +1,27 @@
-@wip
 Given('that I have signed-in') do
   pending # Write code here that turns the phrase above into concrete actions
-  visit '/'
 end
 
-@wip
 And('I am at the dashboard page') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-@wip
 And('my streams have successfully loaded') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-@wip
 And('I make a requisition to list my streams') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-@wip
 And('I receive a connection error') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-@wip
 Then('I expect to see a list of my data streams') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-@wip
 Then('I expect to receive a clear and descriptive message of error') do
   pending # Write code here that turns the phrase above into concrete actions
 end
