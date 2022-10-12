378057e2e1d2fcf42c7f5eaa9ae36ce9007d7b60
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-11 23:27:12 -0300

    feat(sign-in): creating draft steps for sign-in

---
+++ features/step_definitions/sign-in.rb
@@ -0,0 +1,19 @@
+Given('that I have a valid AWS account') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("I am at the sign-in page") do
+    visit "/"
+  end
+  
+  When('I type my {string}') do |field_name| 
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I press the {string} button') do |button_name|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I expect to be redirected to my dashboard') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
