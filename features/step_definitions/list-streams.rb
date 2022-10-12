d770605b0a77b43f098e337d32f905bee56f4d1f
Lucas Vilela <lucas.vilela@quintoandar.com.br>
2022-10-12 00:31:22 -0300

    feat(list-streams): adds draft steps

---
+++ features/step_definitions/list-streams.rb
@@ -0,0 +1,15 @@
+Given('that I have signed-in') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Given('I am at the dashboard page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Given('my streams have successfully loaded') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then('I expect to see a list of my data streams') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
