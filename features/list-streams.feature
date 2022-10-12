ba413c02a666c12cdd37ae727e4dbc773087f6c3
LeonardoMeireles <leo.meireles01@gmail.com>
2022-10-12 19:36:05 -0300

    feat: new scenario

--- features/list-streams.feature
+++ features/list-streams.feature
@@ -3,11 +3,17 @@ Feature: List Streams
     It enables the user to list all streams inside the Amazon Kinesis Streams.
     Further, the user can select one of these streams and manipulate or visualize it.
 
-    Example: you can add one example here if you think it would be useful
-    
     Scenario: Successful List
     
         Given that I have signed-in
         And I am at the dashboard page
         And my streams have successfully loaded
         Then I expect to see a list of my data streams
+
+    Scenario: Infrastructure instability
+
+        Given that I have successfully signed-in
+        And I am on the dashboard page
+        And I make a requisition to list my streams
+        And I receive a connection error
+        Then I expect to receive a clear and descriptive message of error
--- features/step_definitions/list-streams.rb
+++ features/step_definitions/list-streams.rb
@@ -1,20 +1,40 @@
+# Successful List
 Given('that I have signed-in') do
-    @wip
     pending # Write code here that turns the phrase above into concrete actions
     visit '/'
   end
 
-  Given('I am at the dashboard page') do
-    @wip
+  And('I am at the dashboard page') do
     pending # Write code here that turns the phrase above into concrete actions
   end
 
-  Given('my streams have successfully loaded') do
-    @wip
+  And('my streams have successfully loaded') do
     pending # Write code here that turns the phrase above into concrete actions
   end
 
   Then('I expect to see a list of my data streams') do
-    @wip
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+
+# Infrastructure instability
+Given('that I have signed-in') do
+    pending # Write code here that turns the phrase above into concrete actions
+    visit '/'
+  end
+
+  And('I am at the dashboard page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  And('I make a requisition to list my streams') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  And('I receive a connection error') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then('I expect to receive a clear and descriptive message of error') do
     pending # Write code here that turns the phrase above into concrete actions
   end
