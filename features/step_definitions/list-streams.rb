a97aee8cdb0577a4153e56fda60f32b43bee6ea6
LeonardoMeireles <leo.meireles01@gmail.com>
2022-10-12 19:59:15 -0300

    fix: removed duplicate actions

--- features/step_definitions/list-streams.rb
+++ features/step_definitions/list-streams.rb
@@ -1,40 +1,28 @@
-# Successful List
 Given('that I have signed-in') do
-    pending # Write code here that turns the phrase above into concrete actions
-    visit '/'
-  end
+  pending # Write code here that turns the phrase above into concrete actions
+  visit '/'
+end
 
-  And('I am at the dashboard page') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+And('I am at the dashboard page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
-  And('my streams have successfully loaded') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+And('my streams have successfully loaded') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
-  Then('I expect to see a list of my data streams') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+And('I make a requisition to list my streams') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
+And('I receive a connection error') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
-# Infrastructure instability
-Given('that I have signed-in') do
-    pending # Write code here that turns the phrase above into concrete actions
-    visit '/'
-  end
+Then('I expect to see a list of my data streams') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
-  And('I am at the dashboard page') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-
-  And('I make a requisition to list my streams') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-
-  And('I receive a connection error') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-
-  Then('I expect to receive a clear and descriptive message of error') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Then('I expect to receive a clear and descriptive message of error') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
