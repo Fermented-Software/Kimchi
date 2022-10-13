e54e2e879aebcb53616b562e504f6416823701b5
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-13 20:11:25 -0300

    feat(sign-in): Improving feature and steps for story

--- features/sign-in.feature
+++ features/sign-in.feature
@@ -1,12 +1,41 @@
 Feature: Sign-in
     As a User
     I want to access the platform with my credentials
-    So that i can see my streamed messages
+    So that i can see my data streams
+
+    Scenario: Successful User Sign-up
+        Given I am at the sign-up page
+        When I fill the field "Username" with "Miguel Vasconcelos"
+        When I fill the field "Password" with "abcdefg"
+        When I fill the field "AWS Key ID" with "AKIAIOSFODNN7EXAMPLE"
+        When I fill the field "AWS Key Secret" with "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+        And click on the "Sign-up" button
+        Then the data should have been persisted at a database
+        And I should be redirected to the home page
+
+    Scenario: Unsuccessful User Sign-up (Empty username)
+        Given I am at the sign-up page
+        When leave the field "Username" empty
+        And click on the "Sign-up" button
+        Then the error message "Please fill in a password" should show up
+
+    Scenario: Unsuccessful User Sign-up (Empty password)
+        Given I am at the sign-up page
+        When I fill the field "Username" with "Miguel Vasconcelos"
+        And leave the field "Password" empty
+        And click on the "Sign-up" button
+        Then the error message "Please fill in a password" should show up
 
     Scenario: Successful Sign-in
-        Given that I have a valid AWS account
-        And I am at the sign-in page
-        When I type my "AWS Access Key"
-        And I type my "AWS Secret Key"
-        And I press the "login" button
-        Then I expect to be redirected to my dashboard
+        Given that I have valid login
+        And I am at the home page
+        When I fill the field "Username" with "Miguel Vasconcelos"
+        And I fill the field "password" with "abcdefg"
+        And click on the "Sign-in" button
+        Then I should be redirected to my dashboard
+
+    Scenario: Logged-in user should see their dashboard
+        Given that I have valid login
+        When I have already signed in
+        And I am at the home page
+        Then I should be redirected to my dashboard
--- features/step_definitions/sign-in.rb
+++ features/step_definitions/sign-in.rb
@@ -1,19 +1,51 @@
-Given('that I have a valid AWS account') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Given("I am at the sign-in page") do
-    visit "/"
-  end
-  
-  When('I type my {string}') do |field_name| 
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I press the {string} button') do |button_name|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('I expect to be redirected to my dashboard') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Given("I am at the sign-in page") do
+  visit "/sign-in"
+end
+
+When('click on the {string} button') do |button_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should to be redirected to my dashboard') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am at the sign-up page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I fill the field {string} with {string}') do |field, value|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the data should have been persisted at a database') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be redirected to the home page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('leave the field {string} empty') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the error message {string} should show up') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('that I have valid login') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am at the home page') do
+  visit "/"
+end
+
+When('I have already signed in') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be redirected to my dashboard') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
