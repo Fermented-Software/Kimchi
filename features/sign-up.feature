4c6813b3b64996d6ff14c75d51e9979599e9f99b
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-16 19:06:18 -0300

    feat: Remove scenarios unrelated to user story

--- features/sign-up.feature
+++ features/sign-up.feature
@@ -11,7 +11,7 @@ Feature: Sign-in
         When I fill the field "AWS Key Secret" with "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
         And click on the "Sign-up" button
         Then the data should have been persisted at a database
-        And I should be redirected to the home page
+        And I should be redirected to my dashboard
 
     Scenario: Unsuccessful User Sign-up (Empty username)
         Given I am at the sign-up page
@@ -25,17 +25,3 @@ Feature: Sign-in
         And leave the field "Password" empty
         And click on the "Sign-up" button
         Then the error message "Please fill in a password" should show up
-
-    Scenario: Successful Sign-in
-        Given that I have valid login
-        And I am at the home page
-        When I fill the field "Username" with "Miguel Vasconcelos"
-        And I fill the field "password" with "abcdefg"
-        And click on the "Sign-in" button
-        Then I should be redirected to my dashboard
-
-    Scenario: Logged-in user should see their dashboard
-        Given that I have valid login
-        When I have already signed in
-        And I am at the home page
-        Then I should be redirected to my dashboard
--- features/step_definitions/sign-up.rb
+++ features/step_definitions/sign-up.rb
@@ -1,12 +1,4 @@
-Given("I am at the sign-in page") do
-  visit "/sign-in"
-end
-
 When('click on the {string} button') do |button_name|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('I should to be redirected to my dashboard') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -22,27 +14,11 @@ Then('the data should have been persisted at a database') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then('I should be redirected to the home page') do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 When('leave the field {string} empty') do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('the error message {string} should show up') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given('that I have valid login') do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given('I am at the home page') do
-  visit "/"
-end
-
-When('I have already signed in') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
