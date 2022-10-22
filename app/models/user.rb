82dee976af8714312e26c23b6e4ef72bd8e6873a
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-22 12:15:03 -0300

    fix: fix user email validation on the wrong field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User
   field :password, type: String
   field :aws_key, type: String
   field :aws_secret, type: String
-  validates :email, presence: { message: "Please fill in your e-mail"}
-  validates :password,
-              presence: { message: "Please fill in your password"},
+  validates :email,
+              presence: { message: "Please fill in your e-mail"},
               format: { with: URI::MailTo::EMAIL_REGEXP, message: "Please fill in a valid e-mail" }
+  validates :password, presence: { message: "Please fill in your password"}
 end
