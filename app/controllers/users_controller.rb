d7a99a0f8d88d066c2d22d2d140e3ad60d372cf6
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-24 10:19:38 -0300

    feat: adding route for showing an specific user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
+  # GET /users/1 or /users/1.json
+  def show
+  end
+
   # GET /users/new
   def new
     @user = User.new
