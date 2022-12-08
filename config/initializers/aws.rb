d8ea315e54725f0efeae7cd49486f91d6852803c
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-12-07 21:24:37 -0300

    fix: make aws initializer only if not in prod env

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,10 @@
 require 'aws-sdk-kinesis'
 
-Aws.config.update(
-  endpoint: 'http://localhost:4566',
-  :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], 
-  region: 'us-east-1',
-)
+if (ENV["RAILS_ENV"] != "production")
+  Aws.config.update(
+    endpoint: 'http://localhost:4566',
+    :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], 
+    region: 'us-east-1',
+  )
+end
